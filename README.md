e3bf798eca22dab65b98168ddb830af19822fa50
Timo <43245438+TheRisenPhoenix@users.noreply.github.com>
2024-03-20 08:56:06 +0100

    Small adjustements

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-Steps to take:
+A few notes to get started:
 - Clone the yolov5 repository (https://github.com/ultralytics/yolov5) into src/yolov5
-- (optional) adjust the paths in data/guidewiredetection_clinic.yaml to your training data (or better: create a new yaml file)
-- Run the `run_experiment.sh` script for training and validation. Of course, you can also adjust the steps and hyperparameters there
+- Adjust the paths in data/guidewiredetection_clinic.yaml to your training data (or better: create a new yaml file, make sure to change the paths in the `run_experiment.sh` script as well)
+- Run the `run_experiment.sh` script for training and validation. Of course, you can also adjust the steps and hyperparameters there.
+- This only shows how to work with yolo and how training was done. This is not intended to be used for a final project.
--- src/run_experiment.sh
+++ src/run_experiment.sh
@@ -1,22 +1,7 @@
-#export COMET_API_KEY=sRP5pkOCVbBvNJk2kgwOyw9OQ
-#export COMET_WORKSPACE=mochaab
-#export COMET_PROJECT_NAME=yolov5experiments
-#export COMET_AUTO_LOG_GRAPH=True
-#export COMET_AUTO_LOG_PARAMETERS=True
-#export COMET_AUTO_LOG_HISTOGRAM_WEIGHTS=True
-#export COMET_AUTO_LOG_METRICS=True
-#export COMET_AUTO_LOG_ENV_DETAILS=True
-#export COMET_MAX_IMAGE_PREDICTIONS=200
-#export COMET_EVAL_BATCH_LOGGING_INTERVAL=1
-#export COMET_EVAL_LOG_CONFUSION_MATRIX=True
-#export COMET_MAX_IMAGE_UPLOADS=200
-#export COMET_LOG_PREDICTIONS=True
-#export COMET_LOGGING_FILE={user}.log
-
 # $bbox_interval=1
 # $save_period=1
-epoch_clinic=1 #000
-epoch_lab=1 #000
+epoch_clinic=1000
+epoch_lab=1000
 
 # 1. CLINICAL DATA
 echo "1. TRAIN ON CLINICAL DATASET"
@@ -25,15 +10,13 @@ newest_folder_train=$(ls -lt "../../runs/train/" | grep '^d' | head -n 1 | awk '
 echo "Trained weights: $newest_folder_train"
 
 echo "2. VALIDATION ON TEST DATASET"
-python yolov5/val.py --weights runs/train/$newest_folder_train/weights/best.pt --data "../data/guidewiredetection_clinic.yaml" --img 640 --task test --conf-thres 0.5 --save-hybrid
+python yolov5/val.py --weights runs/train/"$newest_folder_train"/weights/best.pt --data "../data/guidewiredetection_clinic.yaml" --img 640 --task test --conf-thres 0.5 --save-hybrid
 
 echo "3. VALIDATION ON VAL DATASET"
-python yolov5/val.py --weights runs/train/$newest_folder_train/weights/best.pt --data "../data/guidewiredetection_clinic.yaml" --img 640 --conf-thres 0.5 --save-hybrid
+python yolov5/val.py --weights runs/train/"$newest_folder_train"/weights/best.pt --data "../data/guidewiredetection_clinic.yaml" --img 640 --conf-thres 0.5 --save-hybrid
 
 echo "4. DETECT ON TEST DATASET"
-# python detect.py --weights runs/train/$newest_folder_train/weights/best.pt --source '../data/CUSTOM_FLUO_W_BG/images/test/SR_p2_og_hf_vf_rot90_colch_34.png'
-python yolov5/detect.py --weights runs/train/$newest_folder_train/weights/best.pt --source '../data/CLINIC_TEST_SR_100/images/'
-#python yolov5/detect.py --weights runs/train/$newest_folder_train/weights/best.pt --source '../../data/fluoroscopytestdata/'
+python yolov5/detect.py --weights runs/train/"$newest_folder_train"/weights/best.pt --source '../data/CLINIC_TEST_SR_100/images/'
 
 
 
@@ -44,12 +27,10 @@ newest_folder_train_auto=$(ls -lt "runs/train/" | grep '^d' | head -n 1 | awk '{
 echo "Trained weights: $newest_folder_train_auto"
 
 echo "2. VALIDATION ON TEST DATASET"
-python yolov5/val.py --weights runs/train/$newest_folder_train_auto/weights/best.pt --data "../data/guidewiredetection_lab.yaml" --img 640 --task test --conf-thres 0.5 --save-hybrid
+python yolov5/val.py --weights runs/train/"$newest_folder_train_auto"/weights/best.pt --data "../data/guidewiredetection_lab.yaml" --img 640 --task test --conf-thres 0.5 --save-hybrid
 
 echo "3. VALIDATION ON VAL DATASET"
-python yolov5/val.py --weights runs/train/$newest_folder_train_auto/weights/best.pt --data "../data/guidewiredetection_lab.yaml" --img 640 --conf-thres 0.5  --save-hybrid
+python yolov5/val.py --weights runs/train/"$newest_folder_train_auto"/weights/best.pt --data "../data/guidewiredetection_lab.yaml" --img 640 --conf-thres 0.5  --save-hybrid
 
 echo "4. DETECT ON TEST DATASET"
-# python detect.py --weights runs/train/$newest_folder_train_auto/weights/best.pt --source '../data/CUSTOM_AUTO_W_BG/images/test/GW_p2_hf_vf_saferot_40.png'
-python yolov5/detect.py --weights runs/train/$newest_folder_train_auto/weights/best.pt --source '../data/LAB_TEST_SR_100/images/'
-#python yolov5/detect.py --weights runs/train/$newest_folder_train_auto/weights/best.pt --source '../data/fluoroscopytestdata/'
+python yolov5/detect.py --weights runs/train/"$newest_folder_train_auto"/weights/best.pt --source '../data/LAB_TEST_SR_100/images/'
