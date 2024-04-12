bc5a5ea6b3e9e050cca3d8d7ee597690e5f0db46
rhit-halseysh <halseysh@rose-hulman.edu>
2024-04-12 11:07:14 +0200

    Adding working code

--- src/run_experiment.sh
+++ src/run_experiment.sh
@@ -1,36 +1,36 @@
 # $bbox_interval=1
 # $save_period=1
-epoch_clinic=1000
-epoch_lab=1000
+epoch_clinic=1000 #1000
+epoch_lab=1000 #1000
 
 # 1. CLINICAL DATA
 echo "1. TRAIN ON CLINICAL DATASET"
-python yolov5/train.py --img 640 --batch 64 --epochs $epoch_clinic --data "../data/guidewiredetection_clinic.yaml" --weights yolov5s.pt --bbox_interval 1
-newest_folder_train=$(ls -lt "../../runs/train/" | grep '^d' | head -n 1 | awk '{print $9}')
+python yolov5//train.py --img 640 --batch 64 --epochs $epoch_clinic --data "../data/guidewiredetection_clinic.yaml" --weights yolov5s.pt --bbox_interval 1
+newest_folder_train=$(ls -lt "yolov5/runs/train/" | grep '^d' | head -n 1 | awk '{print $9}')
 echo "Trained weights: $newest_folder_train"
 
 echo "2. VALIDATION ON TEST DATASET"
-python yolov5/val.py --weights runs/train/"$newest_folder_train"/weights/best.pt --data "../data/guidewiredetection_clinic.yaml" --img 640 --task test --conf-thres 0.5 --save-hybrid
+python yolov5/val.py --weights yolov5/runs/train/"$newest_folder_train"/weights/best.pt --data "../data/guidewiredetection_clinic.yaml" --img 640 --task test --conf-thres 0.5 --save-hybrid
 
 echo "3. VALIDATION ON VAL DATASET"
-python yolov5/val.py --weights runs/train/"$newest_folder_train"/weights/best.pt --data "../data/guidewiredetection_clinic.yaml" --img 640 --conf-thres 0.5 --save-hybrid
+python yolov5/val.py --weights yolov5/runs/train/"$newest_folder_train"/weights/best.pt --data "../data/guidewiredetection_clinic.yaml" --img 640 --conf-thres 0.5 --save-hybrid
 
 echo "4. DETECT ON TEST DATASET"
-python yolov5/detect.py --weights runs/train/"$newest_folder_train"/weights/best.pt --source '../data/CLINIC_TEST_SR_100/images/'
+python yolov5/detect.py --weights yolov5/runs/train/"$newest_folder_train"/weights/best.pt --source '../data/CLINIC_TEST_SR_100/images/D_test_sr'
 
 
 
 # 2. LAB DATA
 echo "1. TRAIN ON LAB DATASET"
 python yolov5/train.py --img 640 --batch 64 --epochs $epoch_lab --data "../data/guidewiredetection_lab.yaml" --weights yolov5s.pt --bbox_interval 1
-newest_folder_train_auto=$(ls -lt "runs/train/" | grep '^d' | head -n 1 | awk '{print $9}')
+newest_folder_train_auto=$(ls -lt "yolov5/runs/train/" | grep '^d' | head -n 1 | awk '{print $9}')
 echo "Trained weights: $newest_folder_train_auto"
 
 echo "2. VALIDATION ON TEST DATASET"
-python yolov5/val.py --weights runs/train/"$newest_folder_train_auto"/weights/best.pt --data "../data/guidewiredetection_lab.yaml" --img 640 --task test --conf-thres 0.5 --save-hybrid
+python yolov5/val.py --weights yolov5/runs/train/"$newest_folder_train_auto"/weights/best.pt --data "../data/guidewiredetection_lab.yaml" --img 640 --task test --conf-thres 0.5 --save-hybrid
 
 echo "3. VALIDATION ON VAL DATASET"
-python yolov5/val.py --weights runs/train/"$newest_folder_train_auto"/weights/best.pt --data "../data/guidewiredetection_lab.yaml" --img 640 --conf-thres 0.5  --save-hybrid
+python yolov5/val.py --weights yolov5/runs/train/"$newest_folder_train_auto"/weights/best.pt --data "../data/guidewiredetection_lab.yaml" --img 640 --conf-thres 0.5  --save-hybrid
 
 echo "4. DETECT ON TEST DATASET"
-python yolov5/detect.py --weights runs/train/"$newest_folder_train_auto"/weights/best.pt --source '../data/LAB_TEST_SR_100/images/'
+python yolov5/detect.py --weights yolov5/runs/train/"$newest_folder_train_auto"/weights/best.pt --source '../data/LAB_TEST_SR_100/images/test/'
